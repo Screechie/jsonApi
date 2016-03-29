91a60282154cb7eed1654d1c8a4c8c8c08a80c77
sim <simeonbourne@gmail.com>
2016-03-29 09:50:57 -0400

    Modify server.js

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var qs = require('querystring');//Allow users to perform Get requests using quer
 
 var items = [];
 var root = __dirname;//Get root directory
+
 console.log("root: "+root);
-console.log("url object: "+url);
+// console.log("url object: "+url);
 
 var server = http.createServer(function(request, response){
+
 	//Prevent browser from making favicon request
 	if(request.url === '/favicon.ico'){
 		response.writeHead(200, {"Content-Type" : "image/x-icon" });
@@ -17,19 +19,77 @@ var server = http.createServer(function(request, response){
 		return;
 	}
 	
+	
 	if(request.method == 'GET'){
-		//Parse url for query to obtain specific items from Array
-		var query = url.parse(request.url).query;
 
-		console.log("query: "+query);
+		var reqUrl = request.url;
+		console.log(reqUrl);
+		console.log(typeof root);
+
+		parsedUrl = url.parse(reqUrl);
+		console.log(parsedUrl);
+		console.log( typeof parsedUrl.pathname);
+
+		var path = join(root, parsedUrl.pathname);
+		console.log("path: "+ path);
+
+		//Get all items
+		if(reqUrl = '/'){
+			// for(i=0;i<items.length;i++)
+			// response.write(JSON.stringify({"todo_items": items[i]}));
+
+			var stream = fs.createReadStream(path); 
+			stream.on('data', function (chunk){
+				response.write(chunk);
+			});
+
+			stream.on('end', function () {
+				response.end();
+			});
+		}
+		else{
+			//Get items specified in query string
+		}
+
+		
+
+		// var url = url.parse(request.url);
+
+		// console.log(url);
+
+		//separate by query string
+		var query = url.parse(request.url,true).query;
+
+		//Return all items if a query string is not specified
+		// if(query == ""){
+		// 	for(i=0;i<items.length;i++)
+		// 	response.end(JSON.stringify({"todo_items": items[i]}));
+		// }
+
+		// console.log("query: ");
+		// console.log(query);
+		// console.log(request);
+		// console.log(request.url);
+
+		// //Parse url for query to obtain specific items from Array
+		// var query = url.parse(request.url).query;
+
+		// console.log("query: ");
+		// console.log(query);
 
 		//Use querystring module to parse query and get separate ids
-		var id = qs.parse(query);
-		var item;
-		console.log(id);
-		for(i=0;i<id.length;i++){
-			response.end(JSON.stringify({"todo_items": items[i]}));
-		}
+		// var id = qs.parse(query);
+		// var item;
+		// console.log(id);
+		// for(i=0;i<id.length;i++){
+		// 	response.end(JSON.stringify({"todo_items": items[i]}));
+		// }
+
+
+
+
+
+
 		// var path = join(root, reqUrl.path);
 		//console.log(reqUrl);
 		// console.log(reqUrl.query);
@@ -37,8 +97,9 @@ var server = http.createServer(function(request, response){
 		// console.log(request.rawHeaders[1]+" made a GET request");
 		
 	}
-	else if(request.method == 'POST' && request.url == '/')
-	{
+
+	else if(request.method == 'POST' && request.url == '/'){
+
 		var str = "";
 
 		//necessary to receive data as utf-8 strings or buffer objects will be received instead
@@ -54,48 +115,43 @@ var server = http.createServer(function(request, response){
 			var list_Item = function(){
 				this.itemName = "";
 				this.id = 0;
-			};			
+			};	
 
 			/*Split stream by new line character to get separate
 			TODO items and then push them to the Array*/
 			var item = str.split('\n');
 
 			console.log("The items entered were: ");
-			
+
 			for(i=0; i<item.length; i++){
 
 				//Needs to be inside loop in order for a new object to be created on each iteration
 				var currentItem = new list_Item();
 
-				var id = i + 1;
-
-				console.log("item "+ id + " is " + item[i]);
-				console.log("item " + id + " has and id of " + id);
+				var id = i + 1;				
 
 				//set values of current item for each item
 				currentItem.itemName = item[i];
 				currentItem.id = id;	
-				console.log("current item: ");
-				console.log(currentItem);		
 				items.push(currentItem);
-				// continue;
+			}			
 
-			}
-
-			
-
-			console.log("items: ");
+			console.log("The user entered the following items: ");
 			for(i=0;i<items.length;i++){
 				console.log(items[i]);
 			}
 
+			console.log(JSON.stringify({"todo_items": items}));
 			response.end(JSON.stringify({"todo_items": items}));
 
 		});
 
 	}
+
 	else if(request.method == 'DELETE'){
-		//Parse url to obtain ID to be deleted
+
+		//Parse url to obtain ID to be deleted from query string
+
 		
 	}
 	// else{
@@ -108,6 +164,9 @@ var server = http.createServer(function(request, response){
 	//Add strings received to messages array
 
 });
+
+//Updates
+//http://localhost:3000/items/1/edit
 
 server.listen(3000, function(){
 	console.log("Listening on port 3000");
